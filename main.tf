be8f9c67c28d7b7b1f2e4bfdcd10fbab33ac0bb6
NaveenKumar-0 <snk29133@gmail.com>
2025-07-24 11:27:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-reigon = "us-east-1"
+  region = "us-east-1" 
 }
 
-resource "aws_ec2" "one" {
-ami = "ami-020cba7c55df1f615"
-instance_type = "t2.micro"
-tags = {
-Name = "dev"
-}
+resource "aws_instance" "one" { 
+  ami           = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "dev"
+  }
 }
